c4a3a55cb47080dbf347e5631be17cde4ecfb43a
damanveen <damanveen@gmail.com>
2016-11-03 20:29:49 -0400

    fixed

--- Assignment2.c
+++ Assignment2.c
@@ -13,11 +13,14 @@ process *tail;
 process *ListProcess;
 FILE *file;
 
-int index;
+int i;
 int totalProcesses = 0;
 int totalTurnaroundTime = 0;
 int waitTime = 0;
 time_t start_t, end_t;
+
+
+
 
 struct process{
 	pthread_t tid;
@@ -30,14 +33,12 @@ struct process{
 	process *next;
 };
 
-/*
 void sec_wait(int sec){
 	clock_t wait_till_end;
 	wait_till_end = clock() + sec * CLOCKS_PER_SEC; 
 	while(clock() < wait_till_end){}
 }
- */
-
+ 
 int getNumEntries(){
 	int numProcess = 0;	
 	file = fopen("test.txt", "r");
@@ -48,49 +49,67 @@ int getNumEntries(){
     return numProcess;
 }
 
-void* running(void *index){
-	int a = *((int*) index);
+void* running(void *i){
+	int a = *((int*) i);
 	//printf("Index value %i\n", a);
 	process *NewItem = malloc(sizeof(process));
 
 	while(1){
 		
 		if(curr != NULL && curr->tid == pthread_self()  && curr->added == 1){
-			
+			//printf("%lu\n", pthread_self());
+
 			time_t current_t = time(NULL);
 
-			printf("Thread %d, executing at: %f\n\n", a, difftime(current_t, start_t));
+			printf("Thread %d is executing at: %f\n\n", a, difftime(current_t, start_t));
+			//printf("Thread is running (Sleeping).\n");
 
-			sleep(curr->cpu_time/1000);	
-			
+			sleep(curr->cpu_time/1000);
 			current_t = time(NULL);
 
-			printf("Thread is done running at: %f\n\n", difftime(current_t, start_t));
+			printf("Thread %d is done running at: %f\n",a, difftime(current_t, start_t));
 
 			curr->turnaround_time = difftime(current_t, start_t) - curr->arrival_time/1000;
 			totalTurnaroundTime = totalTurnaroundTime + curr->turnaround_time;
 		
-			printf("Thread turnaround time is: %f\n", curr->turnaround_time);
-			printf("Thread waittime is: %d\n\n", waitTime);
+			
+
+
+			printf("Thread %d turnaround time is: %f\n", a, curr->turnaround_time);
+			printf("Thread %d waittime is: %d\n\n", a, waitTime);
 
 			waitTime = waitTime + (curr->cpu_time/1000);
 			totalProcesses++;
+
+
+
 			curr = curr->next;
+
+
 			return NULL;
 		}
 
-
 		time_t current_t = time(NULL);
+		
+		
+
+		//if(ListProcess[a].arrival_time/1000 == t){
+
+		//	printf("%d, %f\n", ListProcess[a].arrival_time/1000, t);
+
+		//}
 		
 		if(ListProcess[a].arrival_time/1000 == difftime(current_t, start_t) && NewItem->added == 0){
 			
+
 			NewItem->tid = pthread_self();
 			NewItem->arrival_time = ListProcess[a].arrival_time;
 			NewItem->cpu_time = ListProcess[a].cpu_time;
 			NewItem->next = NULL;
+
+
 			
  			if(curr == NULL){
-				 
 				curr = NewItem;
 				tail = NewItem;
 			}
@@ -100,7 +119,9 @@ void* running(void *index){
 				tail = tail->next;	
 			}
 
-			printf("%d was added to the ready queue at %f\n", a, difftime(current_t, start_t));
+			printf("%d was added to the ready queue at %f\n\n", a, difftime(current_t, start_t));
+
+
 			NewItem->added = 1;
 		}
 
@@ -130,17 +151,19 @@ int main(){
 	}
 	fclose(file);
 	
+	for(i = 0; i < number; i++){
 		//Add method of changing pointer slower
-		printf("Creating thread. . . \n");
-		*arg = index;
+		printf("Creating thread %i. . . \n\n", i);
+		*arg = i;
 		ListProcess[i].added = 0;
 		pthread_create(&ListProcess[i].tid, NULL, (void*)running, arg);
 		
 		
 		sleep(1);
+
 	}
     
-	for(index = 0; index < number; i++){
+	for(i = 0; i < number; i++){
 		pthread_join(ListProcess[i].tid,NULL);
 	}
     
@@ -160,3 +183,4 @@ int main(){
 	printf("Throughput: %f   5 Processes/second\n", totalProcesses/difftime(end_t, start_t));
 	exit(0);
 
+}
